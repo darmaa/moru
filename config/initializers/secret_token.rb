d8006533554df8bb14590b00e769b44cd476c133
Zolzaya Erdenebaatar <zolzaya.erdenebaatar@gmail.com>
2011-10-16 21:49:13 +0800

    added rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moru::Application.config.secret_token = '261a8b5eb92c2f571c5291b704540d9119188f3906efce5ae761fd063f010465e328e303a06e2eb30ff43de586cdeeae12b07671f411d871e0e454775a760c1c'
