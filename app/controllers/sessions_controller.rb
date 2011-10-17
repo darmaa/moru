1829071ceaa390c76b151bc7e06649c216863d02
Zolzaya Erdenebaatar <zolzaya.erdenebaatar@gmail.com>
2011-10-18 00:31:22 +0800

    fixed some errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user] = nil
+    session[:user_id] = nil
     redirect_to root_url :notice => "Signed out!"
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Moru::Application.routes.draw do
   # OpenID
   match "/auth/:provider/callback" => "sessions#create"
   match "/auth/failure" => "sessions#failure"
-  match "/signout" => "session#destroy", :as => :signout
+  match "/signout" => "sessions#destroy", :as => :signout
   match "/signin" => "sessions#new", :as => :signin
 
   # You can have the root of your site routed with "root"
