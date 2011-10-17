699b814b76c3add84f3d48f77ed4582ad13cc567
Zolzaya Erdenebaatar <zolzaya.erdenebaatar@gmail.com>
2011-10-18 00:24:02 +0800

    added authentication function to application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,18 @@
 class SessionsController < ApplicationController
   def create
-    raise request.env["omniauth.auth"].to_yaml
+    auth = request.env["omniauth.auth"]
+    user = User.where(:provider=>auth['provider'], :uid=>auth['uid']).first || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_url :notice => "Signed in!"
   end
 
   def failure
+    redirect_to root_url, :alert => "Authentication failed. Please try again!"
   end
 
   def destroy
+    session[:user] = nil
+    redirect_to root_url :notice => "Signed out!"
   end
 
   def new
